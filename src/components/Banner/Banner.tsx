7a1732c3afade1e1e039b0a87b7866736e5fb62b
ArtemiyAntnamve <143155099+ArtemiyAntnamve@users.noreply.github.com>
2024-07-24 16:34:58 +0300

    migrated to ts

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -1,8 +1,13 @@
 import { formatTimeAgo } from '../../helpers/formatTimeAgo'
+import { INews } from '../../interfaces'
 import Image from '../Image/Image'
 import styles from './styles.module.css'
 
-const Banner = ({ item }) => {
+interface Props {
+	item: INews
+}
+
+const Banner = ({ item }: Props) => {
 	return (
 		<div className={styles.banner}>
 			<Image image={item?.image} />
